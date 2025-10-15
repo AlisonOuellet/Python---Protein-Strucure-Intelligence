3f32293d65ae422bf01f28ae4939e80399fbf9af
AlisonOuellet <ALOUE123@ulaval.ca>
2025-10-15 11:17:03 -0400

    fetch, requirements and clean gitignore

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 
+from modules.fetch_pdb import fetch_pdb
+
+if __name__ == "__main__":
+    result = fetch_pdb(["1TUP", "4HHB", "1A8O"])
+    print(result)
--- modules/fetch_pdb.py
+++ modules/fetch_pdb.py
@@ -1,1 +1,14 @@
+# Fetches PDB structures from RCSB PDB and saves them locally.
+from rcsbapi.data import DataQuery as Query
 
+def fetch_pdb(pdb_ids):
+    query = Query(
+        input_type = "entries",
+        input_ids = ["1TUP"],
+        return_data_list=["exptl.method"]
+    )
+
+    return query.exec()
+
+# https://rcsbapi.readthedocs.io/en/latest/
+# https://data.rcsb.org/index.html#data-api 
