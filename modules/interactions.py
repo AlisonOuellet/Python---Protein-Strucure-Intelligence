da4781f17c5d9faca11a109c34cafffb6e39d269
AlisonOuellet <ALOUE123@ulaval.ca>
2025-10-15 12:16:21 -0400

    def

--- modules/interactions.py
+++ modules/interactions.py
@@ -1,1 +1,7 @@
+# Detects hydrogen bonds, hydrophobic contacts, and salt bridges in molecular structures.
 
+def detect_hydrogen_bonds():
+
+def detect_hydrophobic_contacts():
+
+def detect_salt_bridges():
